2df5a687ff98d23c520106adc2d1003fdc8a34bf
AnthomasGit <122173943+AnthomasGit@users.noreply.github.com>
2025-11-19 21:09:25 -0600

    Added @RepositoryRestResource(path="members") to change repo endpoint in EmployeeRepository.java.
    Added default page size to 20 in application.properties.
    
    added springdoc openapi ui dependency to pom.xml

--- 03-spring-boot-rest-crud-employee -with-spring-data-jpa/src/main/java/com/Thomas/Project2/demo/dao/EmployeeRepository.java
+++ 03-spring-boot-rest-crud-employee -with-spring-data-jpa/src/main/java/com/Thomas/Project2/demo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.Thomas.Project2.demo.dao;
 
 import com.Thomas.Project2.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
     //that's it, CRUD methods included!!
